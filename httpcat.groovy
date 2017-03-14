44c7fa429ffb3b8e29166c3bd6ec381b5f8e06bc
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2017-03-14 13:27:23 -0700

    Minor changes (untested)

--- httpcat.groovy
+++ httpcat.groovy
@@ -1,3 +1,5 @@
+package test;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -12,40 +14,75 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.json.JSONException;
 
 public class HttpCat {
 
-	@javax.ws.rs.Path("")
-	public static class MyResource { // Must be public
+  @javax.ws.rs.Path("")
+  public static class MyResource { // Must be public
 
-		@GET
-		@javax.ws.rs.Path("")
-		@Produces("application/json")
-		public Response list(@QueryParam("value") String iValue,
-				@QueryParam("key") String iKey,
-				@QueryParam("categoryId") String iCategoryId)
-				throws JSONException, IOException {
-System.err.println("list()");
-System.err.println("Writing to stdout: " + iCategoryId + "::" + System.currentTimeMillis() + "::" + iValue);
-			System.out.println(iCategoryId + "::" + System.currentTimeMillis() + "::" + iValue);
-			return Response.ok().header("Access-Control-Allow-Origin", "*")
-					.type("application/json").build();
-		}
-	}
+    @GET
+    @javax.ws.rs.Path("")
+    @Produces("application/json")
+    public Response list(@QueryParam("value") String iValue, @QueryParam("key") String iKey,
+        @QueryParam("categoryId") String iCategoryId) throws JSONException, IOException {
+      System.err.println("list()");
+      System.err.println("Writing to stdout: " + iCategoryId + "::" + System.currentTimeMillis() + "::" + iValue);
+      System.out.println(iCategoryId + "::" + System.currentTimeMillis() + "::" + iValue);
+      return Response.ok().header("Access-Control-Allow-Origin", "*").type("application/json").build();
+    }
+  }
 
-	public static void main(String[] args) throws URISyntaxException, IOException,
-			KeyManagementException, UnrecoverableKeyException, NoSuchAlgorithmException,
-			KeyStoreException, CertificateException, InterruptedException {
-		try {
-			JdkHttpServerFactory.createHttpServer(new URI("http://localhost:" + args[0] + "/"),
-					new ResourceConfig(MyResource.class));
-		} catch (Exception e) {
-			// e.printStackTrace();
-			System.err.println("Port already listened on.");
-			System.exit(-1);
-		}
-	}
+  public static void main(String[] args)
+      throws URISyntaxException, IOException, KeyManagementException, UnrecoverableKeyException,
+      NoSuchAlgorithmException, KeyStoreException, CertificateException, InterruptedException {
+
+    String port;
+    _parseOptions: {
+
+      Options options = new Options().addOption("p", "port", true, "Here you can set parameter .").addOption("h",
+          "help", false, "show help.");
+
+      Option option = Option.builder("a").longOpt("block-size").desc("use SIZE-byte blocks").hasArg().argName("SIZE")
+          .build();
+      options.addOption(option);
+
+      // This doesn't work
+      // options.addOption(Option.builder("p").argName("p").longOpt("port").required().build());
+
+      try {
+        CommandLine cmd = new DefaultParser().parse(options, args);
+        port = cmd.getOptionValue("p", "4499");
+        System.out.println("CommandLineOptionsExample.parse() - SRIDHAR: port = " + port);
+        System.out.println("CommandLineOptionsExample.parse() - SRIDHAR: a = " + cmd.getOptionValue("a"));
+
+        if (cmd.hasOption("h")) {
+          // This prints out some help
+          HelpFormatter formater = new HelpFormatter();
+
+          formater.printHelp("Main", options);
+          System.exit(0);
+        }
+      } catch (ParseException e) {
+        e.printStackTrace();
+      }
+    }
+
+    try {
+      JdkHttpServerFactory.createHttpServer(new URI("http://localhost:" + args[0] + "/"),
+          new ResourceConfig(MyResource.class));
+    } catch (Exception e) {
+      // e.printStackTrace();
+      System.err.println("Port already listened on.");
+      System.exit(-1);
+    }
+  }
 }
