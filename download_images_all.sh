9f1e689805ab46b6f96663954bc4c1184058ecfe
ssarnobat <ss401533@gmail.com>
2019-01-22 13:06:07 -0800

    Do not source control huge file.

--- download_images_all.sh
+++ download_images_all.sh
@@ -9,4 +9,4 @@ cat ~/sarnobat.git/yurl_queue_httpcat.txt | grep -i -P '(JPG|PNG|GIF)' | perl -p
 touch ~/sarnobat.git/db/auto/yurl_queue_httpcat_images_downloaded.txt ; comm -2 -3  <(cat ~/sarnobat.git/db/auto/yurl_queue_httpcat_images.txt | sort) <(cat ~/sarnobat.git/db/auto/yurl_queue_httpcat_images_downloaded.txt | perl -pe 's{::.*}{}g' | sort) | grep -v '^data:' | tee ~/sarnobat.git/db/auto/yurl_queue_httpcat_images_undownloaded.txt
 
 # Download them, remove them from the input file, and record which have been downloaded in the output file
-cd ~/github/httpcat && groovy download_file_from_list.groovy  ~/sarnobat.git/db/auto/yurl_queue_httpcat_images_undownloaded.txt /media/sarnobat/3TB/new/move_to_unsorted/images ~/sarnobat.git/db/auto/yurl_queue_httpcat_images_downloaded.txt | tee -a ~/sarnobat.git/db/auto/yurl_queue_httpcat_images_downloaded_unreliable.txt
+cd ~/github/httpcat && groovy download_file_from_list.groovy  ~/sarnobat.git/db/auto/yurl_queue_httpcat_images_undownloaded.txt /media/sarnobat/3TB/new/move_to_unsorted/images ~/sarnobat.git/db/auto/yurl_queue_httpcat_images_downloaded.txt | tee -a /tmp/yurl_queue_httpcat_images_downloaded_unreliable.txt
