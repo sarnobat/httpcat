5e504a71623df78c7074eecb592b31292d7d1777
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-04 23:19:36 -0700

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 ## 2022-07-29
-/usr/local/Cellar/openjdk@11/11.0.12/bin/java -classpath "$HOME/github/groovy_libraries/.groovy/lib/*" ~/github/httpcat/HttpcatPure.java 4466 2>&1 | tee  /tmp/out.log
+
+## Work
+
+
+## Air
+/usr/local/Cellar/openjdk@11/11.0.12/bin/java -classpath "$HOME/github/groovy_libraries/.groovy/lib/*" ~/github/httpcat/HttpcatPure.java 4466 2>&1 | tee  /tmp/out.log | grep  --line-buffered http | xargs -n 1 -d'\n' open
