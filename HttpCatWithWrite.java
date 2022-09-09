ab297d3d6371c4ade4ce7bd0791d9d64efeccdc4
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-08 20:58:07 -0700

    Minor changes (untested)

--- HttpCatWithWrite.java
+++ HttpCatWithWrite.java
@@ -21,40 +21,40 @@ import org.json.JSONException;
 
 public class HttpCatWithWrite {
 
-  @javax.ws.rs.Path("")
-  public static class MyResource { // Must be public
+	@javax.ws.rs.Path("")
+	public static class MyResource { // Must be public
 
-    @GET
-    @javax.ws.rs.Path("")
-    @Produces("application/json")
-    public Response list(@QueryParam("value") String iValue, @QueryParam("key") String iKey,
-        @QueryParam("categoryId") String iCategoryId) throws JSONException, IOException {
-      System.err.println("list()");
-      String line = iCategoryId + "::" + System.currentTimeMillis() + "::" + iValue;
-      System.err.println("Writing to stdout: " + line);
-      // I wish I didn't have to do this in Java but I found that even though
-      // the browser was returning success, nothing was getting written to the
-      // file.
-      System.err.println("[DEBUG] about to write to file: " + filepath);
-      FileUtils.write(Paths.get(filepath).toFile(), line + "\n", true);
-      System.err.println("[DEBUG] wrote to file");
-      System.out.println(line);
-      return Response.ok().header("Access-Control-Allow-Origin", "*").type("application/json").build();
-    }
-  }
+		@GET
+		@javax.ws.rs.Path("")
+		@Produces("application/json")
+		public Response list(@QueryParam("value") String iValue, @QueryParam("key") String iKey,
+		                     @QueryParam("categoryId") String iCategoryId) throws JSONException, IOException {
+			System.err.println("list()");
+			String line = iCategoryId + "::" + System.currentTimeMillis() + "::" + iValue;
+			System.err.println("Writing to stdout: " + line);
+			// I wish I didn't have to do this in Java but I found that even though
+			// the browser was returning success, nothing was getting written to the
+			// file.
+			System.err.println("[DEBUG] about to write to file: " + filepath);
+			FileUtils.write(Paths.get(filepath).toFile(), line + "\n", true);
+			System.err.println("[DEBUG] wrote to file");
+			System.out.println(line);
+			return Response.ok().header("Access-Control-Allow-Origin", "*").type("application/json").build();
+		}
+	}
 
-  private static String filepath = System.getProperty("user.home") + "/db.git/yurl_flatfile_db/yurl_queue_httpcat.txt";
+	private static String filepath = System.getProperty("user.home") + "/db.git/yurl_flatfile_db/yurl_queue_httpcat.txt";
 
-  public static void main(String[] args)
-      throws URISyntaxException, IOException, KeyManagementException, UnrecoverableKeyException,
-      NoSuchAlgorithmException, KeyStoreException, CertificateException, InterruptedException {
-    
-    try {
-      JdkHttpServerFactory.createHttpServer(new URI("http://localhost:" + 4465 + "/"), new ResourceConfig(MyResource.class));
-    } catch (Exception e) {
-      System.err.println("Port already listened on: 4465");
-      System.exit(-1);
-    }
-            System.err.println("CommandLineOptionsExample.parse() - SRIDHAR: 6");
-  }
+	public static void main(String[] args)
+	throws URISyntaxException, IOException, KeyManagementException, UnrecoverableKeyException,
+	NoSuchAlgorithmException, KeyStoreException, CertificateException, InterruptedException {
+
+		try {
+			JdkHttpServerFactory.createHttpServer(new URI("http://localhost:" + 4465 + "/"), new ResourceConfig(MyResource.class));
+		} catch (Exception e) {
+			System.err.println("Port already listened on: 4465");
+			System.exit(-1);
+		}
+		System.err.println("CommandLineOptionsExample.parse() - SRIDHAR: 6");
+	}
 }
